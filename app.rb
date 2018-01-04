bd5a39d8436928c2bd76042352f4f3af07be454a
Hoon Lee <yl.hoony@gmail.com>
2018-01-04 06:43:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Hoon"
+  end
+
+  get '/hometown' do
+    "My hometown is Seoul"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Gangnam Style"
+  end
+
 end
